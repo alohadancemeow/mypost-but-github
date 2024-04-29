45900c8131eec5abae9d4ef0f7cf416eb8c17416
Alohadancemeow <nawongsak@gmail.com>
2024-04-29 15:48:12 +0700

    ✅ update next.config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['images.unsplash.com', 'img.clerk.com'],
+        unoptimized: true
+    }
+}
 
 module.exports = nextConfig
